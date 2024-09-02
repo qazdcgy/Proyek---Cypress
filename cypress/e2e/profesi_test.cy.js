737a99a1636715594ee70beae5b02e435b2676d5
qazdcgy <newbie2346@gmail.com>
2024-09-02 14:37:32 +0700

    C01

--- cypress/e2e/profesi_test.cy.js
+++ cypress/e2e/profesi_test.cy.js
@@ -2,31 +2,29 @@ describe("Master -> Profesi", () => {
   beforeEach(() => {
     cy.loginCookie();
     cy.visit("https://hcm-dev.trustmedis.net");
-  });
+    cy.wait(5000);
 
-  it("Tambah Data Profesi", () => {
     cy.contains("button", "Master").click();
     cy.wait(5000);
     cy.contains("button", "Profesi").scrollIntoView().click();
   });
 
-  // it("Tambah Data Profesi", () => {
+  it("Tambah Data Profesi", () => {
+    cy.contains("Tambah Profesi").click();
+    cy.wait(2000);
 
-  //   cy.contains("Tambah Profesi").click();
-  //   cy.wait(2000);
+    cy.get('[data-path="profesi_nama"]').type("Tech QA");
+    cy.get('[data-path="profesi_jenis"]').click();
 
-  //   cy.get('[data-path="profesi_nama"]').type("Tech QA");
-  //   cy.get('[data-path="profesi_jenis"]').click();
+    cy.get(".mantine-ScrollArea-root")
+      .should("be.visible")
+      .contains("Medis")
+      .click();
 
-  //   cy.get(".mantine-ScrollArea-root")
-  //     .should("be.visible")
-  //     .contains("Medis")
-  //     .click();
-
-  //   cy.get('[data-path="profesi_keterangan"]').type("Quality Assurance");
-  //   cy.contains("Simpan").click();
-  //   cy.wait(6000);
-  // });
+    cy.get('[data-path="profesi_keterangan"]').type("Quality Assurance");
+    cy.contains("Simpan").click();
+    cy.wait(6000);
+  });
 
   it("Ubah Data Profesi", () => {
     cy.get(".mantine-Checkbox-input").eq(5).check();
@@ -46,16 +44,15 @@ describe("Master -> Profesi", () => {
     cy.wait(6000);
   });
 
-  // it("Hapus data Profesi", () => {
-  //   cy.get(".mantine-Checkbox-input").eq(5).check();
-  //   cy.contains("Hapus").click();
-  //   cy.wait(2000);
+  it("Hapus data Profesi", () => {
+    cy.get(".mantine-Checkbox-input").eq(5).check();
+    cy.contains("Hapus").click();
+    cy.wait(2000);
 
-  //   cy.get(".mantine-Modal-body")
-  //     .should("be.visible")
-  //     .and("contain", "Hapus Data");
+    cy.get(".mantine-Modal-body")
+      .should("be.visible")
+      .and("contain", "Hapus Data");
 
-  cy.get('button[data-variant="outline"][type="submit"]')
-    .contains("Logout")
-    .click();
+    cy.get('button[aria-label="Delete"]').click();
+  });
 });
